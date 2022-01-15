536b0fd0ce81c32ea99847b6a7b8d092fb7a2f22
Elias Olate <elias.or.dev@gmail.com>
2022-01-15 00:28:08 -0300

    first commit

---
+++ ConsolaLog/Log.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConsolaLog
+{
+    public class Log
+    {
+        private string Path = "";
+
+        public Log(string path) {
+            this.Path = path;
+        }
+
+        public void Add(string sLog) {
+            //comprobar si el directorio existe
+            CreateDirectory();
+
+            string nombre = GetNameFile();
+            string cadena = "";
+            cadena += DateTime.Now + " - " + sLog  + Environment.NewLine;
+
+            StreamWriter sw = new StreamWriter(Path+"/"+nombre, true);
+            sw.Write(cadena);
+            sw.Close();
+        }
+
+        private string GetNameFile() {
+            string nombre = "";
+            nombre = "log_" + DateTime.Now.Year + "_" + DateTime.Now.Month + "_" + DateTime.Now.Day + ".txt";
+            return nombre;
+        }
+
+        private void CreateDirectory() {
+            try
+            {
+                if (!Directory.Exists(Path))
+                    Directory.CreateDirectory(Path);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ ConsolaLog/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsolaLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log log = new Log(@"C:\Users\elias\source\repos\log");
+            log.Add("Hola Mundo");
+        }
+    }
+}
